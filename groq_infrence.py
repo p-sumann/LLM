2c58fa9e384d307650216c72f4fd2fad097ffd24
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-16 23:25:33 +0545

    groq infrence

--- groq_infrence.py
+++ groq_infrence.py
@@ -1,29 +1,50 @@
-from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.document_loaders.web_base import WebBaseLoader
+import streamlit as st
+import os
 from langchain_groq import ChatGroq
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings
-from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+import time
 
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.environ['GROQ_API_KEY']
 
 load_dotenv()
 
 laoder = WebBaseLoader('https://bitskraft.com/career/')
 docs = laoder.load()
 
-splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
+splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
 
 documents = splitter.split_documents(docs)
 
-ollama_emb = OllamaEmbeddings(
-    model="llama:7b",
-)
+embeddings = OllamaEmbeddings()
 
-vector_db = FAISS.from_documents(documents, embedding=ollama_emb)
+vector_db = FAISS.from_documents(documents, embedding=embeddings)
 
 
-llm = chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+llm = chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768", groq_api_key=groq_api_key)
 
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+
+retrieval_chain.invoke({"input":prompt})
